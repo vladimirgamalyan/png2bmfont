a50f9c36d92a951853d4167535a9fd35ef608300
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2015-07-13 16:57:52 +0700

    Add boost to CMakeLists.txt and .travis.yml.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# png2bmfont
+﻿# png2bmfont
 Convert png glyphs to [bmfont](http://www.angelcode.com/products/bmfont/) compatible texture/fnt files.
 
 This software use [RectangleBinPack](https://github.com/juj/RectangleBinPack) by [Jukka Jylänki](https://github.com/juj).
--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 #include <iostream>
 
-using namespace std;
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
-int main()
+int main( int argc, char* argv[] )
 {
-    cout << "Hello, World!" << endl;
+    fs::path d;
+    po::options_description desc( "Allowed options" );
+    desc.add_options()
+            ( "help", "produce help message" )
+            ( "dir", po::value< fs::path >( &d ), "glyphs directory" );
+    po::variables_map vm;
+    po::store( po::parse_command_line( argc, argv, desc ), vm );
+
+    if ( vm.count( "help" ) )
+    {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    po::notify( vm );
+
     return 0;
 }
